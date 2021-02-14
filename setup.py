46cfeb0ebaa457884b5bb60d81ae8351b57a365a
Hank Barta <hbarta@gmail.com>
2021-02-14 09:29:49 -0600

    added Python 3.9

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
 		'Programming Language :: Python :: 3.3',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.9',
 		'Programming Language :: Python :: Implementation :: PyPy',
 		'Topic :: Software Development :: Libraries',
 	],
