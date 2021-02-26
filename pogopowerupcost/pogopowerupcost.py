59a8a96c4e5cce31b9338b5bb0af1bc9048d332d
Hank Barta <hbarta@gmail.com>
2021-02-25 23:27:01 -0600

    shadow cost, API tweak

--- pogopowerupcost/pogopowerupcost.py
+++ pogopowerupcost/pogopowerupcost.py
@@ -7,7 +7,7 @@ Strategy to eliminate redundant lines.
 - calculate the correct key for a given level
 """
 
-COST_PER_POWERUP = {
+NORMAL_COST_PER_POWERUP = {
 	# level: [stardust, candy]
 	2.5:  [  200,  1],
 	4.5:  [  400,  1],
@@ -31,7 +31,31 @@ COST_PER_POWERUP = {
 	40.5: [10000, 15], # Unconfirmed
 }
 
-def calculate_powerup_cost(from_level, to_level, cost_table=COST_PER_POWERUP):
+SHADOW_COST_PER_POWERUP = {
+	# level: [stardust, candy]
+	2.5:  [  240,   2],
+	4.5:  [  480,   2],
+	6.5:  [  720,   2],
+	8.5:  [  960,   2],
+	10.5: [ 1200,   2],
+	12.5: [ 1560,   3],
+	14.5: [ 1920,   3],
+	16.5: [ 2280,   3],
+	18.5: [ 2640,   3],
+	20.5: [ 3000,   3],
+	22.5: [ 3600,   4],
+	24.5: [ 4200,   4],
+	26.5: [ 4800,   5],
+	28.5: [ 5400,   5],
+	30.5: [ 6000,   8],
+	32.5: [ 7200,  10],
+	34.5: [ 8400,  10],
+	36.5: [ 9600,  12],
+	38.5: [ 10800, 15],
+	40.5: [ 12000, 18], # Unconfirmed
+}
+
+def calculate_powerup_cost(from_level, to_level, cost_table=NORMAL_COST_PER_POWERUP):
 	total_stardust = 0
 	total_candy = 0
 	while from_level < to_level:
