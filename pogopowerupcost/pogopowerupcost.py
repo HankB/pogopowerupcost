d748a030318d4a833e879a3c687b90012e4a029c
Hank Barta <hbarta@gmail.com>
2021-02-25 22:24:26 -0600

    factor out table incex calculation

--- pogopowerupcost/pogopowerupcost.py
+++ pogopowerupcost/pogopowerupcost.py
@@ -31,11 +31,15 @@ COST_PER_POWERUP = {
 	40.5: [10000, 15], # Unconfirmed
 }
 
+def get_table_key(level):
+	return level-((level+1) % 2)+1.5
+	x =    level-((level+1) % 2)+1.5
+	
 def calculate_powerup_cost(from_level, to_level, cost_table=COST_PER_POWERUP):
 	total_stardust = 0
 	total_candy = 0
 	while from_level < to_level:
-		table_key = from_level-((from_level+1) % 2)+1.5
+		table_key = get_table_key(from_level)
 		stardust, candy = cost_table[table_key]
 		total_stardust += stardust
 		total_candy += candy
